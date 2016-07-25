099215fa39694e202186d2126aea40d4a1230ed3
stefems <kowalmax.s@gmail.com>
2016-07-25 00:01:01 -0400

    fixed the db issue, showing titles!

--- html/cal.php
+++ html/cal.php
@@ -7,29 +7,32 @@
 			//Connecting to DB
 			$db = mysqli_connect('localhost','web','webplebnafets2015','denverted_events') or die('Error connecting to MySQL server.');
 			//Composing test query
-			//$query = "SELECT eventTime, ageLimit, eventTitle, eventOpeners, eventVenue, eventLink, eventCost FROM event_table0";
-			$query = "select eventOpeners from event_table0";
+			$query = "SELECT eventTime, ageLimit, eventTitle, eventOpeners, eventVenue, eventLink, eventCost FROM event_table0";
 			mysqli_query($db, $query) or die('Error querying database.');
 			//Composing query to show all results
 			$result = mysqli_query($db, $query);
-			$jsonTest = array();
+			$eventTime = $ageLimit = $eventTitle = $eventOpeners = $eventVenue = $eventLink = $eventCost = array();
 			//if the number of rows recevied is not 0
 			if (mysqli_num_rows($result) > 0) {
 				//this runs for each row in the DB
 				while ($row = mysqli_fetch_array($result))
 				{
-					$jsonTest[] = $row[0];
+					//make sure the row is utf8
+					$row = utf8_converter($row);
+					$eventTime[] = $row[0];
+					$ageLimit[] = $row[1];
+					$eventTitle[] = $row[2];
+					$eventOpeners[] = $row[3];
+					$eventVenue[] = $row[4];
+					$eventLink[] = $row[5];
+					$eventCost[] = $row[6];
 				}
-				$jsonTest = utf8_converter($jsonTest);
-			}
-			else {
-				echo 'failed';
 			}
 			function utf8_converter($array)
 			{
 				array_walk_recursive($array, function(&$item, $key){
 					if(!mb_detect_encoding($item, 'utf-8', true)){
-							$item = utf8_encode($item);
+						$item = utf8_encode($item);
 					}
 				});
 				return $array;
@@ -37,8 +40,29 @@
 			
 		?>
 		<script type="text/javascript">
-			var result = <?php echo json_encode($jsonTest);?>;
-			alert(result);
+			var eventTime = <?php echo json_encode($eventTime);?>;
+			var ageLimit = <?php echo json_encode($ageLimit);?>;
+			var eventTitle = <?php echo json_encode($eventTitle);?>;
+			var eventOpeners = <?php echo json_encode($eventOpeners);?>;
+			var eventVenue = <?php echo json_encode($eventVenue);?>;
+			var eventLink = <?php echo json_encode($eventLink);?>;
+			var eventCost = <?php echo json_encode($eventCost);?>;
+			//array for objects
+			var eventList = new Array(eventTitle.length);
+			//for all of the items in the output array
+			for (i = 0; i < eventTitle.length; i++)
+			{
+				//create an event object and send it to the array at the same index
+				eventList[i] = {
+					title: eventTitle[i],
+					time: eventTime[i],
+					age: ageLimit[i],
+					openers: eventOpeners[i],
+					venue: eventVenue[i],
+					link: eventLink[i],
+					cost: eventCost[i]
+				};
+			}
 			//How to access a title from the array
 			//alert(eventList[0].title);
 		</script>
@@ -62,45 +86,11 @@
 		
 	</div>
 	<!-- Content Section-->
-	<div class="contentDiv">
+	<div id="contentSection" class="contentDiv">
 		<!--MONTH
 		TODO: Fixed changes for the upcoming month-->
 		<!--EVENT -->
-		<div >A
-		</div>
-		<!--EVENT -->
-		<div >B
-		</div>
-		<!--EVENT -->
-		<div >C
-		</div>
-		<!--EVENT -->
-		<div >A
-		</div>
-		<!--EVENT -->
-		<div >B
-		</div>
-		<!--EVENT -->
-		<div >C
-		</div>
-		<!--EVENT -->
-		<div >A
-		</div>
-		<!--EVENT -->
-		<div >B
-		</div>
-		<!--EVENT -->
-		<div >C
-		</div>
-		<!--EVENT -->
-		<div >A
-		</div>
-		<!--EVENT -->
-		<div >B
-		</div>
-		<!--EVENT -->
-		<div >C
-		</div>
+		
 	</div>
 	<!---------------------------------------->
 
--- html/js/denvertedScripts.js
+++ html/js/denvertedScripts.js
@@ -14,3 +14,22 @@ function unanimateHome() {
 	//get the filter <i> and reset the class
 	document.getElementById("homeIcon").className = "fa fa-arrow-down fa-5x";
 }
+
+document.body.onload = loadEvents;
+
+function loadEvents () { 
+
+	//for each object in eventList
+	for (i = 0; i < eventList.length; i++) {
+		// create a new div element 
+		// and give it some content 
+		var newDiv = document.createElement("div"); 
+		var newContent = document.createTextNode(eventList[i].title); 
+		newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+		// add the newly created element and its content into the DOM 
+		var contentDiv = document.getElementById("contentSection"); 
+		contentDiv.appendChild(newDiv); 
+	}
+	
+}
